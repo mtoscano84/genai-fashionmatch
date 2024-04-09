8e0dc1f4d6332ed8c7186a5126ded4b7f039b2e7
Miguel Toscano <101990367+mtoscano84@users.noreply.github.com>
2024-04-09 14:56:38 +0200

    Update deploy_frontend_to_cloudrun.sh

--- fashionmatch-service/frontend/cloudrun/deploy_frontend_to_cloudrun.sh
+++ fashionmatch-service/frontend/cloudrun/deploy_frontend_to_cloudrun.sh
@@ -3,7 +3,7 @@ GCLOUD_BIN=`which gcloud`
 LOG_DATE=`date`
 echo "###########################################################################################"
 echo "${LOG_DATE} Deploying image to Cloud Run....."
-"${GCLOUD_BIN}" run deploy ${FRONTED_SERVICE_NAME} --source . --platform managed --region ${REGION} --allow-unauthenticated
+"${GCLOUD_BIN}" run deploy ${FRONTEND_SERVICE_NAME} --source . --platform managed --region ${REGION} --allow-unauthenticated
 
 LOG_DATE=`date`
 echo "###########################################################################################"
